38bce82fcf521b1127413b254679e3f344c8cafc
singhmahipal <mahipalsingh052025@gmail.com>
2026-07-19 17:16:01 +0530

    created auth, task Context; implement data storing in the localStorage;

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Login from "./components/Auth/Login.jsx";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard.jsx";
 import AdminDashboard from "./components/Dashboard/AdminDashboard.jsx";
+import { setLocalStorage } from "./utils/localStorage.jsx";
 
 const App = () => {
+  useEffect(() => {
+    setLocalStorage();
+  });
+
   return (
     <>
       {/* <Login /> */}
