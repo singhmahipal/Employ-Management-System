da915869f9e34debf159e67b481a04cba2b75319
singhmahipal <mahipalsingh052025@gmail.com>
2026-07-23 22:49:03 +0530

    implement logout feature

--- src/components/other/Header.jsx
+++ src/components/other/Header.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 
 const Header = ({data}) => {
+  const LogOutUser = () => {
+    localStorage.removeItem("LoggedInUser");
+    window.location.reload()
+  }
+
   return (
     <div className="flex items-end justify-between">
       <h1 className="text-2xl font-medium">
         Hello <br />
         <span className="text-3xl font-semibold">{data ? data.firstName : "Admin" } 👋</span>
       </h1>
-      <button className="bg-red-600 py-2 px-5 font-medium text-base rounded-sm">
+      <button className="bg-red-600 py-2 px-5 font-medium text-base rounded-sm" onClick={LogOutUser}>
         Logout
       </button>
     </div>
